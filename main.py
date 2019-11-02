9b0432a3b34387c7c95c519045566830c0051211
eurashin <eura.shin@gmail.com>
2019-11-02 14:32:18 -0400

    hi

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import datetime
 import tools
 from flask import Flask, render_template, request, jsonify
 import datetime
+from find.imageSearch import imageSearch
+from pagify.pagify1 import pagify
 
 app = Flask(__name__)
 
--- test.py
+++ test.py
@@ -1,22 +1,11 @@
 import tools 
 import datetime
+from find.imageSearch import imageSearch
+from pagify.pagify1 import pagify
 
 df = tools.parse_location_data('LocationHistory.json')
 start = datetime.datetime(2019,7, 1).date()
 end = datetime.datetime(2019, 7, 30).date()
 time = tools.extract_time_period(df, start, end)
-print(time.shape)
-import tools
-import datetime
-from find.imageSearch import imageSearch
-from pagify.pagify1 import pagify
-
-
-# df = tools.parse_location_data('LocationHistory.json')
-# start = datetime.datetime(2019,7, 1).date()
-# end = datetime.datetime(2019, 7, 30).date()
-# time = tools.extract_time_period(df, start, end)
-
-def main(time):
-    paths = imageSearch(time)
-    pagify(paths)
+paths = imageSearch(time)
+pagify(paths)
