40def3091a1d956a88e50c7e7c10ac7e94a78188
Daiwei Lu <daiwei.lu@vanderbilt.edu>
2019-11-02 12:02:50 -0500

    updated main

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from find.imageSearch import imageSearch
 from pagify.pagify1 import pagify
 
 
-def main():
-    df = tools.parse_location_data('LocationHistory.json')
-    start = datetime.datetime(2019,7, 1).date()
-    end = datetime.datetime(2019, 7, 30).date()
-    time = tools.extract_time_period(df, start, end)
-    # print(time)
+# df = tools.parse_location_data('LocationHistory.json')
+# start = datetime.datetime(2019,7, 1).date()
+# end = datetime.datetime(2019, 7, 30).date()
+# time = tools.extract_time_period(df, start, end)
 
+def main(time):
     paths = imageSearch(time)
-    # print(paths)
     pagify(paths)
-
-
-# main()
